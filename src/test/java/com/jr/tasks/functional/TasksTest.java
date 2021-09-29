a831569c8d1e07424c3904cec9f39c8ea1a312ca
v4lb3r <3cifhzht@duck.com>
2021-09-29 09:23:28 -0300

    removal test

--- src/test/java/com/jr/tasks/functional/TasksTest.java
+++ src/test/java/com/jr/tasks/functional/TasksTest.java
@@ -129,4 +129,27 @@ public class TasksTest {
             driver.quit();
         }
     }
+
+    @Test
+    public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+        WebDriver driver = acessarAplicacao();
+
+        try {
+            // add task
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+            driver.findElement(By.id("saveButton")).click();
+            String message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Success!", message);
+
+            // remove task
+            driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+            message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Success!", message);
+        } finally {
+            // quit browser
+            driver.quit();
+        }
+    }
 }
